5fa06134ae780b66e7f36bacd054d9316c1e9eb3
Tejas Parikh <t.parikh@northeastern.edu>
2018-01-17 19:36:25 -0500

    initial commit

---
+++ webapp/src/main/java/com/csye6225/spring2018/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.csye6225.spring2018;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringBootWebApplication.class);
+  }
+
+}
+
