3feb2eb39f8cfc4e6cb0918561b5de2794aedec6
yichuan <yczneu@gmail.com>
2018-01-27 15:46:32 -0500

    add backend controllers and spring security

--- webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
+++ webapp/src/main/java/com/csye6225/spring2018/SpringBootWebApplication.java
@@ -2,7 +2,11 @@ package com.csye6225.spring2018;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.csye6225.spring2018.repository")
+@EntityScan(basePackages = "com.csye6225.spring2018.pojo")
 @SpringBootApplication
 public class SpringBootWebApplication {
 
