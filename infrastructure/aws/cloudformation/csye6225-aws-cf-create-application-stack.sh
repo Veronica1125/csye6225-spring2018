3cc857af55b3440df4c3d8e8825d353a5c1eda4f
yichuan <yczneu@gmail.com>
2018-02-12 17:39:01 -0500

    update create-application-stack

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -0,0 +1,24 @@
+set -e
+#Author Yichuan Zhang
+echo "Author: Yichuan Zhang"
+echo "	      zhang.yichu@husky.neu.edu"
+#Usage: Taking STACK_NAME as parameter and building a vpc, internet gateway, route table and route through aws cloudformation
+
+STACK_NAME=$1
+
+#Create Stack
+aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json
+#Check Stack Status
+STACK_STATUS=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text`
+
+#Wait until stack completely created
+echo "Please wait..."
+
+while [ $STACK_STATUS != "CREATE_COMPLETE" ]
+do
+	STACK_STATUS=`aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[][ [StackStatus ] ][]" --output text`
+done
+
+#Job Done!
+echo "Job Done!"
+
