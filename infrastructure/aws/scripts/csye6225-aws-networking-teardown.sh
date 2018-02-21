0f06365ccca1cdd5fb4d993505452ec42f306238
Xiao Li <li.xiao5@northeastern.edu>
2018-02-20 20:03:30 -0500

    script

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -1,8 +1,8 @@
 set -e
 
-#Author: Yang Yuan
-echo "Author: Yang Yuan"
-echo "        yuan.yang@husky.neu.edu"
+#Author: Xiao Li
+echo "Author: Xiao Li"
+echo "        li.xiao5@husky.neu.edu"
 #Usage: Deleting our networking resources such as Virtual Private Cloud (VPC), Internet Gateway, Route Table and Routes
 
 #Arguments: STACK_NAME
